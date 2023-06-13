06b8172fce4388eacecfc3a82bccfc93a9a76c17
NJUCO <122616369+NESTLEJUCO@users.noreply.github.com>
2023-06-13 10:30:05 -0600

    Print Game Board Added
    Print Game Board Added

--- SODV1202-Connect4FINALPROJECT-NESTLE_JUCO/Program.cs
+++ SODV1202-Connect4FINALPROJECT-NESTLE_JUCO/Program.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿//SODV1202 Connect Four Game Final Term Project
+//Submitted By: Nestle Juco
+using System;
 
 namespace SODV1202FinalProject
 {
     class Connect4TwoPlayerHuman
     {
+        private const int Rows = 6;
+        private const int Columns = 7;
+        private char[,] board;
         private bool isPlayerOnesTurn;
         private Player player1;
         private Player player2;
 
         public Connect4TwoPlayerHuman()
         {
+            board = new char[Rows, Columns];
+            isPlayerOnesTurn = true;
+            InitializeBoard();
             Console.WriteLine("\nConnect 4 Two Player Human vs Human Battle Selected!\n");
             InitializePlayers();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
         }
 
         private void InitializePlayers()
@@ -23,58 +42,80 @@ namespace SODV1202FinalProject
             player1 = new Player(player1Name, 'X');
             player2 = new Player(player2Name, 'O');
         }
-    }
 
-
-
-    class Player
-    {
-        public string Name { get; }
-        public char Symbol { get; }
-
-        public Player(string name, char symbol)
+        private void Play()
         {
-            Name = name;
-            Symbol = symbol;
+                Console.Clear();
+                PrintBoard();
         }
-    }
 
-    class Program
-    {
-        static void Main(string[] args)
+        private void PrintBoard()
         {
-            Console.WriteLine("Welcome to Connect Four Game!\n");
-            Console.WriteLine("Please select game mode below:\n");
-            Console.WriteLine("1 - Human vs Human");
-            Console.WriteLine("2 - Human vs AI Bot");
-            Console.WriteLine("3 - Exit\n");
-
-            int sel;
-            bool isValidSelection = false;
-
-            while (!isValidSelection)
+            Console.WriteLine("Connect Four Game Project\n");
+            for (int row = 0; row < Rows; row++)
             {
-                Console.Write("Key in Selection (1-3): ");
-                string select = Console.ReadLine();
-                sel = int.Parse(select);
+                for (int col = 0; col < Columns; col++)
+                {
+                    Console.Write($"| {board[row, col]} ");
+                }
+                Console.WriteLine("|");
+            }
 
-                if (sel == 1)
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("  1   2   3   4   5   6   7  ");
+            Console.WriteLine("-----------------------------");
+        }
+
+        class Player
+        {
+            public string Name { get; }
+            public char Symbol { get; }
+
+            public Player(string name, char symbol)
+            {
+                Name = name;
+                Symbol = symbol;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Connect Four Game!\n");
+                Console.WriteLine("Please select game mode below:\n");
+                Console.WriteLine("1 - Human vs Human");
+                Console.WriteLine("2 - Human vs AI Bot");
+                Console.WriteLine("3 - Exit\n");
+
+                int sel;
+                bool isValidSelection = false;
+
+                while (!isValidSelection)
                 {
-                    Connect4TwoPlayerHuman game = new Connect4TwoPlayerHuman();
-                    isValidSelection = true;
-                }
-                else if (sel == 2)
-                {
-                    Console.WriteLine("Sorry, this game mode is not yet available.");
-                    isValidSelection = true;
-                }
-                else if (sel == 3)
-                {
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Selection. Please try again.\n");
+                    Console.Write("Key in Selection (1-3): ");
+                    string select = Console.ReadLine();
+                    sel = int.Parse(select);
+
+                    if (sel == 1)
+                    {
+                        Connect4TwoPlayerHuman game = new Connect4TwoPlayerHuman();
+                        game.Play();
+                        isValidSelection = true;
+                    }
+                    else if (sel == 2)
+                    {
+                        Console.WriteLine("Sorry, this game mode is not yet available.");
+                        isValidSelection = true;
+                    }
+                    else if (sel == 3)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Selection. Please try again.\n");
+                    }
                 }
             }
         }
